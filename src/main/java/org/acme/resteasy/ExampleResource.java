7909e1e53f7d193fa6f9726850fed516f5c5cc34
Sergio Canales <sergio.canales.e@gmail.com>
2020-11-21 11:04:13 -0300

    Status in endpoint

--- src/main/java/org/acme/resteasy/ExampleResource.java
+++ src/main/java/org/acme/resteasy/ExampleResource.java
@@ -1,16 +1,41 @@
 package org.acme.resteasy;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/rest/hello")
+@Path("/rest")
 public class ExampleResource {
 
     @GET
+    @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
+    }
+
+    @GET
+    @Path("/status")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String status() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");  
+        LocalDateTime now = LocalDateTime.now();  
+        return dtf.format(now);  
+        
+    }
+
+    @POST
+    @Path("/instatus")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String instatus(String in) {
+        // stuff to do
+        return in;
     }
 }
--- src/test/java/org/acme/resteasy/ExampleResourceTest.java
+++ src/test/java/org/acme/resteasy/ExampleResourceTest.java
@@ -12,7 +12,7 @@ public class ExampleResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/resteasy/hello")
+          .when().get("/rest/hello")
           .then()
              .statusCode(200)
              .body(is("hello"));
