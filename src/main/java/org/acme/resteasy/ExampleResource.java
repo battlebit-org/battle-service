d421fd8253a98ac5982c827fa930a73b8ad4c749
Sergio Canales <sergio.canales.e@gmail.com>
2020-11-21 01:02:24 -0300

    default endpoint hello

--- src/main/java/org/acme/resteasy/ExampleResource.java
+++ src/main/java/org/acme/resteasy/ExampleResource.java
@@ -5,7 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/resteasy/hello")
+@Path("/rest/hello")
 public class ExampleResource {
 
     @GET
